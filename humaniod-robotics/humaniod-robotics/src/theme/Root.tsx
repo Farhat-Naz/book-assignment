dd54f96735c5db2fdcc360ea13e92d07cca8939b
Asif Iqbal <asfiqbl177@gmail.com>
2025-12-07 12:38:54 +0500

    Fix TypeScript typing for Root component

--- humaniod-robotics/humaniod-robotics/src/theme/Root.tsx
+++ humaniod-robotics/humaniod-robotics/src/theme/Root.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Chatbot from '@site/src/components/Chatbot';
 
 // This component wraps the entire app and is a good place to add global components
-export default function Root({children}) {
+export default function Root({children}: {children: React.ReactNode}): JSX.Element {
   return (
     <>
       {children}
